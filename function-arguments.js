dd5a2ba6bb634ca1a278ddd128e4968d2a492787
Kristy Gislason <kristygislason@gmail.com>
2022-08-31 23:06:23 -0700

    Final 3 exercises in javascripting for functions and scope

---
+++ function-arguments.js
@@ -0,0 +1,8 @@
+const math = (x, y, z) => {
+
+  // return the value obtained from multiplying the  
+  // second and third arguments and adding that result to the first argument.  
+  return (y * z) + x;
+};
+
+console.log(math(53, 61, 67));
